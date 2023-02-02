09a95ff17daf4e23e296a6e0238698f2bd8c7b05
Gairick52 <110962676+Gairick52@users.noreply.github.com>
2023-02-02 18:30:27 +0530

    Delete urls.py

--- polls/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-from django.conf.urls import patterns, url
-from polls import views
-
-urlpatterns = url(r'^polls', 'polls.views.index', name='index'),
